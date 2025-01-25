c615a98a3256f0921a800761878cb319ec85246a
Miguel <miguelkunzer567@gmail.com>
2025-01-25 18:28:06 -0300

    Query and Routes params

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const server = express();
+
+// Query params
+// http://localhost:8080/hello?nome=Miguel&idade=19
+// Query params = ?nome=Miguel&idade=19
+server.get('/hello',(req,res)=>{
+    const {nome,idade} = req.query;
+    return res.json({
+        title:'Hello World',
+        message: `Ola ${nome} meu caro`,
+        idade: idade,
+    });
+});
+
+// Routes Params
+// htpp://localhost:8080/hello/Miguel
+// Route Params = /hello/:nome
+server.get('/hello/:nome/:idade',(req,res)=>{
+    const {nome,idade} = req.params;
+
+    return res.json({
+        title:'Hello World',
+        message: `Ola ${nome} meu caro`,
+        idade: idade
+    });
+});
+
+server.listen(8080);
+
